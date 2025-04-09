1d98b4ab9cc9bfd7a8a2df16607849b089d3f68b
lhy <2722562862@qq.com>
2025-04-09 21:44:52 +0800

    搁置的提交

---
+++ src/main/java/com/autoMotiveMes/dto/user/UpdateUserRequestDto.java
@@ -0,0 +1,14 @@
+package com.autoMotiveMes.dto.user;
+
+import lombok.Data;
+
+/**
+ * 实现功能【修改用户入参】
+ *
+ * @author li.hongyu
+ * @date 2025-04-08 20:04:05
+ */
+@Data
+public class UpdateUserRequestDto {
+
+}
---
+++ src/main/java/com/autoMotiveMes/utils/CommonUtils.java
@@ -0,0 +1,27 @@
+package com.autoMotiveMes.utils;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * 实现功能【通用工具类】
+ *
+ * @author li.hongyu
+ * @date 2025-04-08 23:10:09
+ */
+public class CommonUtils {
+
+    // 返回当前用户的用户名
+    public static String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 检查是否存在有效认证且非匿名用户
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            return authentication.getName();
+        }
+        return null;
+    }
+}
