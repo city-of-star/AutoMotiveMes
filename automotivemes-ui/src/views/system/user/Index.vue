d62b9430298d424fca0e2ab01cf3429c631c6d5c
li.hongyu <2722562862@qq.com>
2025-04-10 19:10:07 +0800

    优化application配置

--- automotivemes-ui/src/views/system/user/Index.vue
+++ automotivemes-ui/src/views/system/user/Index.vue
@@ -66,11 +66,11 @@
       </div>
 
       <div class="btn-container">
-        <el-button v-if="hasPermission('system:system:add')"  :icon="Plus" :color=theme_color plain>新增</el-button>
-        <el-button v-if="hasPermission('system:system:update')"  :icon="Edit" :color=btn_update_color plain>修改</el-button>
-        <el-button @click="deleteUser()" v-if="hasPermission('system:system:delete')"  :icon="Delete" :color=btn_delete_color plain>删除</el-button>
-        <el-button v-if="hasPermission('system:system:import')"  :icon="Download" :color=btn_import_color plain>导入</el-button>
-        <el-button v-if="hasPermission('system:system:export')"  :icon="Upload" :color=btn_export_color plain>导出</el-button>
+        <el-button v-if="hasPermission('system:user:add')"  :icon="Plus" :color=theme_color plain>新增</el-button>
+        <el-button v-if="hasPermission('system:user:update')"  :icon="Edit" :color=btn_update_color plain>修改</el-button>
+        <el-button v-if="hasPermission('system:user:delete')"  :icon="Delete" :color=btn_delete_color plain @click="deleteUser()">删除</el-button>
+        <el-button v-if="hasPermission('system:user:import')"  :icon="Download" :color=btn_import_color plain>导入</el-button>
+        <el-button v-if="hasPermission('system:user:export')"  :icon="Upload" :color=btn_export_color plain>导出</el-button>
       </div>
 
       <el-table v-loading="loading" class="table-container" :data="tableData">
