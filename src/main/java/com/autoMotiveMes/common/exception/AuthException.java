eb6403e1ed835b74f30b841516a4a4e7f1579b13
li.hongyu <2722562862@qq.com>
2025-04-21 09:37:58 +0800

    添加serialVersionUID

--- src/main/java/com/autoMotiveMes/common/exception/AuthException.java
+++ src/main/java/com/autoMotiveMes/common/exception/AuthException.java
@@ -1,5 +1,7 @@
 package com.autoMotiveMes.common.exception;
 
+import java.io.Serial;
+
 /**
  * 实现功能【自定义权限验证异常】
  *
@@ -7,5 +9,8 @@ package com.autoMotiveMes.common.exception;
  * @date 2025-03-30 15:23:34
  */
 public class AuthException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 2828370446023896259L;
+
     public AuthException(String message) { super(message); }
 }
